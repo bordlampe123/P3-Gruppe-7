3cbdd3001ff6af61f70ef8e84b8302ff61c37558
Benjamin Buurgaard Jørgensen <bebj2792@gmail.com>
2023-09-05 20:32:35 +0200

    Nu kan den klare farver

--- Benjas_Lort/Opencv_Test1.py
+++ Benjas_Lort/Opencv_Test1.py
@@ -3,10 +3,10 @@
 
 import numpy as np
 import cv2 as cv
-#img = cv.imread("C:/Users/bebj2/OneDrive/Skrivebord/Ordnede billeder/Bund1.png", -1)
-img = cv.imread("C:/Users/bebj2/Downloads/tinypic.png", -1)
+img = cv.imread("C:/Users/bebj2/OneDrive/Skrivebord/Billede/TestRed1.png", 1)
+#img = cv.imread("C:/Users/bebj2/Downloads/tinypic.png", -1)
 assert img is not None, "file could not be read, check with os.path.exists()"
-[x,y] = img.shape
+print(img.shape)
 #img = cv.resize(img, (0,0), fx=1/5, fy=1/5)
 #img = cv.resize(img, (200,200))
 #img = cv.rotate(img, cv.ROTATE_90_COUNTERCLOCKWISE)
