5e0a7041dd5383242c9191eaf0b7940df9fd19e7
BurningSegway <urkanin@gmail.com>
2023-09-05 11:44:06 +0200

    Seje ting er seje

--- Pierres shit/Computer Vision/1-showPicture.py
+++ Pierres shit/Computer Vision/1-showPicture.py
@@ -1,12 +1,12 @@
 import cv2
 
 # Open picture
-img = cv2.imread("tinypic.png", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("C:/Users/pierr/Desktop/P3-cunts/Pierres shit/Computer Vision/tinypic.png", cv2.IMREAD_GRAYSCALE)
 
 height = img.shape[0]
 width = img.shape[1]
 
-print("Højde på billede: " + height + "Bredde på billede: " + width)
+print("Højde på billede: " + str(height) + "Bredde på billede: " + str(width))
 
 for i in range(width):
     for j in range(height):
