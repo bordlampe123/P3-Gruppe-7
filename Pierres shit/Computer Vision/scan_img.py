99a0188a9a932b01567630fcbcd53dada7732f5b
BurningSegway <urkanin@gmail.com>
2023-09-05 12:17:38 +0200

    Jeg gider ikke gode kommentarer

--- Pierres shit/Computer Vision/scan_img.py
+++ Pierres shit/Computer Vision/scan_img.py
@@ -1,12 +1,13 @@
 import cv2
 
 # Open picture
-img = cv2.imread("C:/Users/pierr/Desktop/P3-cunts/Pierres shit/Computer Vision/tinypic.png", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("C:/Users/pierr/Desktop/P3-cunts/Pierres shit/Computer Vision/finger.jpg", cv2.IMREAD_GRAYSCALE)
+cv2.imshow("Our window", img)
 
 height = img.shape[0]
 width = img.shape[1]
 
-print("Højde på billede: " + str(height) + "Bredde på billede: " + str(width))
+print("Højde på billede: " + str(height) + ", Bredde på billede: " + str(width))
 
 for i in range(width):
     for j in range(height):
