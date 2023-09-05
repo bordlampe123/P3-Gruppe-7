73dc823329ce72951531240220cd0af1a5e6f908
BurningSegway <urkanin@gmail.com>
2023-09-05 11:25:32 +0200

    Tilføjede min mappe fra min desktop til min git

---
+++ Pierres shit/Computer Vision/1-showPicture.py
@@ -0,0 +1,19 @@
+import cv2
+
+# Open picture
+img = cv2.imread("tinypic.png", cv2.IMREAD_GRAYSCALE)
+
+height = img.shape[0]
+width = img.shape[1]
+
+print(height)
+print(width)
+
+for i in range(width):
+    for j in range(height):
+        k = img[i, j]
+        print(k)
+
+# Display the picture
+cv2.imshow("Our window", img)
+cv2.waitKey(0)
