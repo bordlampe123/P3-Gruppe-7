c740a6271c465355857dbd143980030d25cb1e91
BurningSegway <urkanin@gmail.com>
2023-09-05 11:33:15 +0200

    Sheee

--- Pierres shit/Computer Vision/1-showPicture.py
+++ Pierres shit/Computer Vision/1-showPicture.py
@@ -6,8 +6,7 @@ img = cv2.imread("tinypic.png", cv2.IMREAD_GRAYSCALE)
 height = img.shape[0]
 width = img.shape[1]
 
-print(height)
-print(width)
+print("Højde på billede: " + height + "Bredde på billede: " + width)
 
 for i in range(width):
     for j in range(height):
