deffca39bb93390207ca142cc0baf4ee06fb5798
MINIK HEILMANN <minikheilmann1908@gmail.com>
2023-09-19 13:35:26 +0200

    Mean Filter done

--- Grønlænderens Kode/MeanFilter.py
+++ Grønlænderens Kode/MeanFilter.py
@@ -5,23 +5,23 @@ import math
 input = cv2.imread("C:/Users/minik/Desktop/VSCode/lion.jpg")#defining the input image
 
 def meanFilter(input, kernelSize):
-    output = [] #defining the output image
-    kernel = np.ones((kernelSize, kernelSize), dtype=int)/(kernelSize**2) #defining the kernel, creates a kernel consisting of 1/kernelsize^2
-    kernelRadius = kernelSize // 2 #defining the radius of the kernel, which is used later to ensure that the kernel is within the boundaries of the input
-    for y, row in enumerate(input): #for loop that runs through the rows and columns of the input image
-        for x, pixel in enumerate(row): #for loop that runs through the pixels of the input image
-            for color in range(3): #for loop that runs through the colors of the input image, so the different color channels
-                accumulator = 0
-                for ky in range(kernelSize): #for loop that runs through the rows and columns of the kernel
-                    for kx in range(kernelSize): #for loop that runs through the rows and columns of the kernel
-                        pixelX = x + kx - kernelRadius
-                        pixelY = y + ky - kernelRadius
-                        if pixelX 
-                output[y, x, color] = accumulator
-    return output
 
+    output = np.zeros((input.shape[0],input.shape[1],3), dtype=np.uint8) #defining the output image
+    
+    kernel = np.ones((kernelSize, kernelSize), dtype=int)/(kernelSize**2) #defining the kernel, creates a kernel consisting of 1/kernelsize^2
+    kernelRadius = kernelSize // 2 #defining the radius of the kernel, which is used to ensure that the kernel is within the boundaries of the input
+    
+    for y in range(kernelRadius, input.shape[0]-kernelRadius): #for loop that runs through the rows and columns of the input image
+        for x in range(kernelRadius, input.shape[1]-kernelRadius): #for loop that runs through the pixels of the input image
+            for color in range(3): #for loop that runs through the colors of the input image, so the different color channels
+                sum = 0
+                for ky in range(kernel.shape[0]):
+                    for kx in range(kernel.shape[1]):
+                        sum += input[y+ky-kernelRadius, x+kx-kernelRadius, color] * kernel[ky, kx]
+                output[y, x, color] = sum
+    return output
                 
-output = meanFilter(input, 7)
+output = meanFilter(input, 83)
 cv2.imshow('input',input)
 cv2.imshow('output',output)
 cv2.waitKey(0)
