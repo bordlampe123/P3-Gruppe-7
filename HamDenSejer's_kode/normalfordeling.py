d2bdc8ffca62aa4579e963ea98b0245571e04736
NAHD <nicklas.deding@gmail.com>
2023-10-10 10:21:57 +0200

    Update normalfordeling.py

--- HamDenSejer's_kode/normalfordeling.py
+++ HamDenSejer's_kode/normalfordeling.py
@@ -1,13 +1,12 @@
-import num as np
+import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-mean = 2
-sigma = 2
-samples = 1000
+def normal(samples, mean, sigma):
+    sample = np.random.normal(samples, mean, sigma)
+    
+    plt.hist(sample, 100)
 
-sample = np.random.normal(mean, sigma, samples)
+    plt.show()
 
-plt.hist(sample, 100)
-
-plt.show()
+normal(2, 2, 1000)
