ea692e43f37c4ad7cbc2be80a56c259db0f17194
MINIK HEILMANN <minikheilmann1908@gmail.com>
2023-10-17 17:52:40 +0200

    Hmmm muk

--- Grønlænderens Kode/CrownDetect.py
+++ Grønlænderens Kode/CrownDetect.py
@@ -3,11 +3,17 @@ import numpy as np
 import math
 
 # Load the image
-image = cv.imread("Vores_MiniProjekt/King Domino dataset/King Domino dataset/Cropped and perspective corrected boards/10.jpg")
+image = cv.imread("Vores_MiniProjekt/King Domino dataset/King Domino dataset/Cropped and perspective corrected boards/19.jpg")
 cv.imshow("board", image)   
 
 # Split the image into RGB channels
 B, G, R = cv.split(image)
+
+# Subtract the green channel from the red channel - Mortens
+GBSub = cv.subtract(G, B)
+
+# Subtract the blue channel from the red channel - Mortens
+BRSub = cv.subtract(B, R)
 
 # Subtract the red channel from the green channel
 GRSub = cv.subtract(G, R)
@@ -31,15 +37,20 @@ BGBSub = cv.subtract(BSub, BGSub)
 FinalImage = cv.subtract(BGBSub, RBSub)
 #cv.imshow("FinalImage", FinalImage)
 
+BRBSub = cv.subtract(B, RBSub)
+
+# Subtracting BRBSub from BRSub - Mortens
+FinalImage2 = cv.subtract(BRBSub, BRSub)  
+
 # Convert the image to the grayscale color space
 gray_img = cv.merge((FinalImage, FinalImage, FinalImage))
 cv.imshow("gray_img", gray_img)
 
 # Template
-template = cv.imread("C:/Users/minik/Desktop/VSCode/SubtractCrown2.jpg", cv.IMREAD_COLOR)
+template = cv.imread("C:/Users/minik/Desktop/VSCode/Crown_bw.png", cv.IMREAD_COLOR)
 
 # Blurred template
-template2 = cv.GaussianBlur(template, (7, 7), 0)
+#template2 = cv.GaussianBlur(template, (7, 7), 0)
 
 gray_img2 = cv.GaussianBlur(gray_img, (7, 7), 0)
 #v.imshow("template", gray_img2)
@@ -47,7 +58,7 @@ gray_img2 = cv.GaussianBlur(gray_img, (7, 7), 0)
 # Create an array with the templates rotated 90, 180 and 270 degrees
 Templates = [template, cv.rotate(template, cv.ROTATE_90_CLOCKWISE), cv.rotate(template, cv.ROTATE_180), cv.rotate(template, cv.ROTATE_90_COUNTERCLOCKWISE)]
 
-Templates2 = [template2, cv.rotate(template2, cv.ROTATE_90_CLOCKWISE), cv.rotate(template2, cv.ROTATE_180), cv.rotate(template2, cv.ROTATE_90_COUNTERCLOCKWISE)]
+#Templates2 = [template2, cv.rotate(template2, cv.ROTATE_90_CLOCKWISE), cv.rotate(template2, cv.ROTATE_180), cv.rotate(template2, cv.ROTATE_90_COUNTERCLOCKWISE)]
 
 template = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
 
@@ -72,20 +83,20 @@ for i in range(5):
         x_end = (i + 1) * subimage_height
         y_start = j * subimage_width
         y_end = (j + 1) * subimage_width
-        subimage = gray_img2[x_start:x_end, y_start:y_end]
+        subimage = gray_img[x_start:x_end, y_start:y_end]
         subimages.append(subimage)
 
 #Template matching through each subimage
 for i in range(len(subimages)):
-    for j in range(len(Templates2)):
-        res = cv.matchTemplate(subimages[i], Templates2[j], cv.TM_CCOEFF_NORMED)
-        loc = np.where(res >= 0.73)
+    for j in range(len(Templates)):
+        res = cv.matchTemplate(subimages[i], Templates[j], cv.TM_CCOEFF_NORMED)
+        loc = np.where(res >= 0.6)
 
         for pt in zip(*loc[::-1]):
             cv.rectangle(subimages[i], pt, (pt[0] + tw, pt[1] + th), (0, 255, 255), 2)
 
         
-cv.imshow("image", gray_img2)
+cv.imshow("image", gray_img)
 
 cv.waitKey(0)
 
