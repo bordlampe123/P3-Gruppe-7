c3d2284aec6fc59179001ce3d011cee1e3a81303
MINIK HEILMANN <minikheilmann1908@gmail.com>
2023-10-18 21:20:37 +0200

    WE FIND CROWNS

--- Grønlænderens Kode/CrownDetect.py
+++ Grønlænderens Kode/CrownDetect.py
@@ -1,9 +1,10 @@
 import cv2 as cv
 import numpy as np
 import math
+import os
 
 # Load the image
-image = cv.imread("Vores_MiniProjekt/King Domino dataset/King Domino dataset/Cropped and perspective corrected boards/19.jpg")
+image = cv.imread("Vores_MiniProjekt/King Domino dataset/King Domino dataset/Cropped and perspective corrected boards/1.jpg")
 cv.imshow("board", image)   
 
 # Split the image into RGB channels
@@ -65,11 +66,10 @@ gray_img2 = cv.GaussianBlur(gray_img, (7, 7), 0)
 # Create an array with the templates rotated 90, 180 and 270 degrees
 Templates = [template, cv.rotate(template, cv.ROTATE_90_CLOCKWISE), cv.rotate(template, cv.ROTATE_180), cv.rotate(template, cv.ROTATE_90_COUNTERCLOCKWISE)]
 
+template = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
 #Templates = [template, templateL, templateD, templateR]
 
 #Templates2 = [template2, cv.rotate(template2, cv.ROTATE_90_CLOCKWISE), cv.rotate(template2, cv.ROTATE_180), cv.rotate(template2, cv.ROTATE_90_COUNTERCLOCKWISE)]
-
-template = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
 
 # Store width and height of image and template in w and h
 w, h, channels = image.shape
@@ -78,14 +78,11 @@ print(image.shape)
 
 tw, th = template.shape
 
-# Creating an empty array for later scoring
-scores = np.zeros((5, 5))
-
 # Creating a list of all the subimages
 subimage_height = h // 5
 subimage_width = w // 5
 
-subimages = []
+subimages = np.zeros((5,5), dtype=object)
 for i in range(5):
     for j in range(5):
         x_start = i * subimage_height
@@ -93,18 +90,56 @@ for i in range(5):
         y_start = j * subimage_width
         y_end = (j + 1) * subimage_width
         subimage = gray_img[x_start:x_end, y_start:y_end]
-        subimages.append(subimage)
+        
+        subimages[i,j] = subimage
+        # Store the subimage in an array
+
+# Creating an array to store the number of matches for each template in each subimage       
+matches = np.zeros((5,5), dtype=object)
+distance_threshold = 10
 
 #Template matching through each subimage
-for i in range(len(subimages)):
-    for j in range(len(Templates)):
-        res = cv.matchTemplate(subimages[i], Templates[j], cv.TM_CCOEFF_NORMED)
-        loc = np.where(res >= 0.65)
+for i in range(5):
+    for j in range(5):
 
-        for pt in zip(*loc[::-1]):
-            cv.rectangle(subimages[i], pt, (pt[0] + tw, pt[1] + th), (0, 255, 255), 2)
+        SubimageMatches = [] # List of matches for each subimage
 
-        
+        MatchCount = 0 # Number of matches for each template in each subimage
+
+        for k in range(len(Templates)):
+            res = cv.matchTemplate(subimages[i,j], Templates[k], cv.TM_CCOEFF_NORMED)
+            loc = np.where(res >= 0.57)
+            
+            MatchPoints = list(zip(*loc[::-1])) # List of points where the template matches the subimage
+
+            UniquePoints = [] # List of unique points
+
+            for match in MatchPoints:
+                unique = True
+                for entries in UniquePoints:
+                    if math.sqrt((match[0] - entries[0])**2 + (match[1] - entries[1])**2) < distance_threshold:
+                        unique = False
+                        break
+                if unique:
+                    UniquePoints.append(match)
+
+            MatchCount += len(UniquePoints) # Number of matches for each template in each subimage
+
+            for pt in zip(*loc[::-1]):
+                cv.rectangle(subimages[i,j], pt, (pt[0] + tw, pt[1] + th), (0, 255, 255), 2)
+
+        matches[i,j] = MatchCount
+
+
+print(matches)
+
+# Sorting matches and storing them in a list
+
+
+
+
+
+
 cv.imshow("image", gray_img)
 
 cv.waitKey(0)
