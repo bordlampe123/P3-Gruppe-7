3d5b306e71282f91d4038965a659ac70b73af5f1
Pierre Mortensen <urkanin@gmail.com>
2023-10-22 14:29:38 +0200

    Right before implementing picture cycle

--- Pierres Mappe/Kingdomino/TerrainCollector.py
+++ Pierres Mappe/Kingdomino/TerrainCollector.py
@@ -1,18 +1,53 @@
 import cv2
 import numpy as np
 import statistics as st
+import os
+
+picture_folder = "Pierres Mappe/Kingdomino/Billeder"
+
+picture_files = [f for f in os.listdir(picture_folder) if f.endswith(('.jpg', '.png', '.jpeg'))]
+
+
+
+
 
 img = cv2.imread("Pierres Mappe/Kingdomino/Billeder/10.jpg")
 
 img_blurred = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
 img_mean = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+img_mean_RG = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+img_mean_RGB = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+img_mean_BR = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+img_mean_G = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+
+
+
+
 img_old_mean = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
 
 img_spice = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
 img_small = np.zeros((20, 20, 3), np.uint8)
+img_small_RG = np.zeros((20, 20, 3), np.uint8)
+img_small_RGB = np.zeros((20, 20, 3), np.uint8)
+img_small_BR = np.zeros((20, 20, 3), np.uint8)
+img_small_G = np.zeros((20, 20, 3), np.uint8)
+
+
+
+
 img_small_big = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+img_small_big_RG = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+img_small_big_RGB = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+img_small_big_BR = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+img_small_big_G = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+
+
+
 
 id = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+
+map_simplified = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+
 
 
 
@@ -24,8 +59,17 @@ img_outR = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
 img_MeanGray = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
 img_MeanGrayBlurred = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
 
+
 img_Meadows = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
 img_Forest = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+img_Waste = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+img_Field = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+img_Ocean = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+img_Mines = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+
+
+
+
 
 current_id = 1
 
@@ -77,25 +121,25 @@ def Meanifier(A, B, C):
             for yy in range(25):
                 for xx in range(25):
                     #print(starty+yy, startx+xx)
-                    meadfindB.append(A[starty+yy, startx+xx, 0])
-                    meadfindG.append(A[starty+yy, startx+xx, 1])
-                    meadfindR.append(A[starty+yy, startx+xx, 2])
+                    meadfindB.append(A[starty+yy, startx+xx])
+                    #meadfindG.append(A[starty+yy, startx+xx, 1])
+                    #meadfindR.append(A[starty+yy, startx+xx, 2])
                     #print(meadfind)
             medianB = st.mean(meadfindB)
-            medianG = st.mean(meadfindG)
-            medianR = st.mean(meadfindR)
-            C[y, x, 0] = medianB
-            C[y, x, 1] = medianG
-            C[y, x, 2] = medianR
+            #medianG = st.mean(meadfindG)
+            #medianR = st.mean(meadfindR)
+            C[y, x] = medianB
+            #C[y, x, 1] = medianG
+            #C[y, x, 2] = medianR
             #print(median)
             for yy in range(25):
                 for xx in range(25):
-                    B[starty+yy, startx+xx, 0] = medianB
-                    B[starty+yy, startx+xx, 1] = medianG
-                    B[starty+yy, startx+xx, 2] = medianR
+                    B[starty+yy, startx+xx] = medianB
+                    #B[starty+yy, startx+xx, 1] = medianG
+                    #B[starty+yy, startx+xx, 2] = medianR
             meadfindB = []
-            meadfindG = []
-            meadfindR = []
+            #meadfindG = []
+            #meadfindR = []
 
 def OldMeanifier(A, B):
 
@@ -188,13 +232,13 @@ def MeanifierGray(A, B):
 
             meadfind = [0]
 
-def Thresh(A, B, z, t):
+def Thresh(A, B, z, t, c):
     for y in range(A.shape[0]):
         for x in range(A.shape[1]):
-            if z <= A[y, x] <= t:
-                B[y, x] = 255
+            if z <= A[y, x, 0] <= t:
+                B[y, x, 0] = c
             else:
-                B[y, x] = 0
+                B[y, x, 0] = 0
 
 def PutHSV(A):
 
@@ -212,7 +256,7 @@ def PutHSV(A):
     for y in range(5):
         for x in range(5):
             bottomLeftCornerOfText = (x*100,y*100+50)
-            cv2.putText(A,str(hsv[y*100,x*100]), 
+            cv2.putText(A,str(A[y*100,x*100]), 
             bottomLeftCornerOfText, 
             font, 
             fontScale,
@@ -254,49 +298,147 @@ def ignite(y, x, A):
         
         increment()
 
-
-Meanifier(img, img_mean, img_small)
-OldMeanifier(img, img_old_mean)
-MeanifierSmall(img_small, img_small_big)
-
-#MeanFilterColor(img, img_blurred, 10)
 Split(img, img_outB, img_outG, img_outR)
 
 img_GsubB = cv2.subtract(img_outG, img_outB)
 img_GsubBsubR = cv2.subtract(img_GsubB, img_outR)
 img_GsubR = cv2.subtract(img_outG, img_outR)
+img_RsubG = cv2.subtract(img_outR, img_outG)
+img_RsubGsubB = cv2.subtract(img_RsubG, img_outB)
+img_BsubR = cv2.subtract(img_outB, img_outR)
 
-alpha = 10
-beta = 1
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#adjusted = cv2.convertScaleAbs(img_GsubBsubR, alpha=alpha, beta=beta)
-#blurred = cv2.blur(adjusted,(10,10))
+img_gray_inverted = cv2.bitwise_not(img_gray)
+img_I_B = cv2.bitwise_not(img_outB)
+img_I_G = cv2.bitwise_not(img_outG)
+img_I_R = cv2.bitwise_not(img_outR)
 
-#MeanifierGray(adjusted, img_MeanGray)
-#MeanifierGray(blurred, img_MeanGrayBlurred)
 
-#Thresh(img_MeanGray, img_Meadows, 50, 255)
-#Thresh(img_MeanGray, img_Forest, 10, 47)
+img_graysubR = cv2.subtract(img_gray_inverted, img_I_R)
+img_graysubRsubB = cv2.subtract(img_graysubR, img_I_B)
+img_graysubRsubBsubG = cv2.subtract(img_graysubRsubB, img_I_G)
 
-for y in range(img.shape[0]):
-    for x in range(img.shape[1]):
-        img_spice[y, x, 0] = img_GsubB[y, x]
-        img_spice[y, x, 1] = img_GsubBsubR[y, x]
-        img_spice[y, x, 2] = img_outB[y, x]
+img_outGmod = np.zeros((img.shape[0], img.shape[1]), np.uint8)
 
+for y in range(img_outGmod.shape[0]):
+    for x in range(img_outGmod.shape[1]):
+        img_outGmod[y, x] = img_outG[y, x, 0]
+
+alpha = 5 # Contrast control
+beta = 5
+
+
+adjusted = cv2.convertScaleAbs(img_outGmod, alpha=alpha, beta=beta)
+
+
+Meanifier(img_GsubR, img_mean, img_small)
+Meanifier(img_RsubG, img_mean_RG, img_small_RG)
+Meanifier(img_RsubGsubB, img_mean_RGB, img_small_RGB)
+Meanifier(img_BsubR, img_mean_BR, img_small_BR)
+Meanifier(adjusted, img_mean_G, img_small_G)
+
+MeanifierSmall(img_small, img_small_big)
+MeanifierSmall(img_small_RG, img_small_big_RG)
+MeanifierSmall(img_small_RGB, img_small_big_RGB)
+MeanifierSmall(img_small_BR, img_small_big_BR)
+MeanifierSmall(img_small_G, img_small_big_G)
+
+
+
+
+
+#MeanFilterColor(img, img_blurred, 10)
+
+
+Thresh(img_small_big, img_Meadows, 25, 55, 255)
+Thresh(img_small_big, img_Forest, 5, 21, 255)
+Thresh(img_small_big_RG, img_Waste, 5, 19, 255)
+Thresh(img_small_big_RGB, img_Field, 7, 22, 255)
+Thresh(img_small_big_BR, img_Ocean, 80, 255, 255)
+Thresh(img_small_big_G, img_Mines, 100, 180, 255)
+
+for y in range(img_Waste.shape[0]):
+    for x in range(img_Waste.shape[1]):
+        if img_Waste[y, x] == 255:
+            map_simplified[y, x] = 129
+
+for y in range(img_Meadows.shape[0]):
+    for x in range(img_Meadows.shape[1]):
+        if img_Meadows[y, x] == 255:
+            map_simplified[y, x] = 43
+
+for y in range(img_Forest.shape[0]):
+    for x in range(img_Forest.shape[1]):
+        if img_Forest[y, x] == 255:
+            map_simplified[y, x] = 86
+
+for y in range(img_Field.shape[0]):
+    for x in range(img_Field.shape[1]):
+        if img_Field[y, x] == 255:
+            map_simplified[y, x] = 172     
+
+for y in range(img_Ocean.shape[0]):
+    for x in range(img_Ocean.shape[1]):
+        if img_Ocean[y, x] == 255:
+            map_simplified[y, x] = 215 
+
+for y in range(img_Mines.shape[0]):
+    for x in range(img_Mines.shape[1]):
+        if img_Mines[y, x] == 255:
+            map_simplified[y, x] = 255        
+
+
+
+
+""" 
 cv2.imshow("Blue", img_outB)
 cv2.imshow("Green", img_outG)
-cv2.imshow("Red", img_outR)
+cv2.imshow("Gray Inverted", img_gray_inverted)
+cv2.imshow("Gray I % Red ", img_graysubR)
+cv2.imshow("Gray I % Red % Blue ", img_graysubRsubB)
+cv2.imshow("Gray I % Red % Blue % Green ", img_graysubRsubBsubG) """
+
+
+
+
+""" cv2.imshow("Red", img_outR)
 cv2.imshow("Green % Blue", img_GsubB)
 cv2.imshow("Green % Red", img_GsubR)
-cv2.imshow("Green % Blue % Red", img_GsubBsubR)
-cv2.imshow("Spicy", img_spice)
+#cv2.imshow("Green % Blue % Red", img_GsubBsubR)
+cv2.imshow("Red % Green % Blue", img_RsubGsubB) """
+
+cv2.imshow("Forest?", img_Forest)
+cv2.imshow("Meadow?", img_Meadows)
+cv2.imshow("Waste?", img_Waste)
+cv2.imshow("Field?", img_Field)
+cv2.imshow("Ocean", img_Ocean)
+cv2.imshow("Mines", img_Mines)
+
+cv2.imshow("Map Simplified", map_simplified)
+
+
+
+""" cv2.imshow("Contrasted", adjusted)
+
+cv2.imshow("Red Minus Green", img_RsubG)
+cv2.imshow("Blue % Red", img_BsubR) """
+
 
 
 PutHSV(img_small_big)
-cv2.imshow("Small to big", img_small_big)
-PutHSV(img_old_mean)
-cv2.imshow("Old Mean", img_old_mean)
+cv2.imshow("Forest and Meadow val", img_small_big)
+PutHSV(img_small_big_RG)
+cv2.imshow("Waste val", img_small_big_RG)
+PutHSV(img_small_big_G)
+cv2.imshow("Mines val", img_small_big_G)
+PutHSV(img_small_big_RGB)
+cv2.imshow("Field val", img_small_big_RGB)
+PutHSV(img_small_big_BR)
+cv2.imshow("Ocean val", img_small_big_BR)
+#PutHSV(img_old_mean)
+#cv2.imshow("Old Mean", img_old_mean)
+
 
 
 
@@ -316,6 +458,6 @@ cv2.imshow("Old Mean", img_old_mean)
 
 cv2.imshow("Original", img)
 #cv2.imshow("Blurred", img_blurred)
-cv2.imshow("Mean", img_mean)
+#cv2.imshow("Mean", img_mean)
 
 cv2.waitKey(0)
