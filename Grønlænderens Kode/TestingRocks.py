6a6eaa7525e7467b930c6fe07ada5a8c51efc930
MINIK HEILMANN <minikheilmann1908@gmail.com>
2023-11-27 09:48:15 +0100

    TestingRocks

--- Grønlænderens Kode/TestingRocks.py
+++ Grønlænderens Kode/TestingRocks.py
@@ -9,15 +9,50 @@ cv.imshow("Image", image)
 HSVImage = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 H, S, V = cv.split(HSVImage)
 
-cv.imshow("H", H)
-cv.imshow("S", S)
-cv.imshow("V", V)
+#cv.imshow("H", H)
+#cv.imshow("S", S)
+#cv.imshow("V", V)
 
 VHSUB = cv.subtract(V, H)
-cv.imshow("VHSUB", VHSUB)
+#cv.imshow("VHSUB", VHSUB)
 
 VHH = cv.subtract(H, VHSUB)
 cv.imshow("VHH", VHH)
 
-cv.imshow("HSVImage", HSVImage)
+#thresholding
+binary = cv.threshold(VHH, 80, 255, cv.THRESH_BINARY)[1]
+
+contours = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+
+#drawCont = cv.drawContours(image, contours, -1, (0, 255, 0), 2)
+
+cv.imshow("Binary", binary)
+
+#cv.imshow("Contours", drawCont)
+
+
+
+min_area = 2100
+unique_contours = [cnt for cnt in contours if cv.contourArea(cnt) > min_area]
+
+print(len(unique_contours))
+
+drawCont2 = cv.drawContours(image, unique_contours, -1, (0, 255, 0), 2)
+
+cv.imshow("Contours2", drawCont2)
+
+#bounding boxes
+bounding_boxes = [cv.boundingRect(cnt) for cnt in unique_contours]
+
+for pt in bounding_boxes:
+    x, y, w, h = pt
+    cv.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+    
+
+cv.imshow("Bounding boxes", image)
+
+print(bounding_boxes)
+
+
+#cv.imshow("HSVImage", HSVImage)
 cv.waitKey(0)
