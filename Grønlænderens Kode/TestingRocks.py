c4c856b2931e10c2709bfcd4ef839b6c7acf59d5
MINIK HEILMANN <minikheilmann1908@gmail.com>
2023-11-28 13:02:05 +0100

    Further Testing

--- Grønlænderens Kode/TestingRocks.py
+++ Grønlænderens Kode/TestingRocks.py
@@ -9,6 +9,34 @@ cv.imshow("Image", image)
 HSVImage = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 H, S, V = cv.split(HSVImage)
 
+GrayImg = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+#cv.imshow("GrayImg", GrayImg)
+
+ContrastImg = cv.equalizeHist(GrayImg)
+#cv.imshow("ContrastImg", ContrastImg)
+
+Gaussian = cv.GaussianBlur(ContrastImg, (9, 9), 0)
+cv.imshow("Gaussian", Gaussian)
+
+Contrast2 = cv.equalizeHist(Gaussian)
+cv.imshow("Contrast2", Contrast2)
+
+Contrast2Inv = cv.bitwise_not(Contrast2)
+cv.imshow("Contrast2Inv", Contrast2Inv)
+
+#sobelx = cv.Sobel(Contrast2, cv.CV_64F, 1, 0, ksize=3)
+#cv.imshow("Sobelx", sobelx)
+
+#sobely = cv.Sobel(Contrast2, cv.CV_64F, 0, 1, ksize=3)
+#cv.imshow("Sobely", sobely)
+
+Threshold = cv.threshold(Contrast2Inv, 210, 255, cv.THRESH_BINARY)[1]
+cv.imshow("Threshold", Threshold)
+
+#Cannyedge
+Canny = cv.Canny(Contrast2, 210, 220)
+#cv.imshow("Canny", Canny)
+
 #cv.imshow("H", H)
 #cv.imshow("S", S)
 #cv.imshow("V", V)
@@ -17,7 +45,7 @@ VHSUB = cv.subtract(V, H)
 #cv.imshow("VHSUB", VHSUB)
 
 VHH = cv.subtract(H, VHSUB)
-cv.imshow("VHH", VHH)
+#cv.imshow("VHH", VHH)
 
 #thresholding
 binary = cv.threshold(VHH, 80, 255, cv.THRESH_BINARY)[1]
@@ -26,11 +54,9 @@ contours = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
 
 #drawCont = cv.drawContours(image, contours, -1, (0, 255, 0), 2)
 
-cv.imshow("Binary", binary)
+#cv.imshow("Binary", binary)
 
 #cv.imshow("Contours", drawCont)
-
-
 
 min_area = 2100
 unique_contours = [cnt for cnt in contours if cv.contourArea(cnt) > min_area]
