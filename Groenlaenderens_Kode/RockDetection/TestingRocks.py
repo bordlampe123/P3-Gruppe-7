6eeef1c15d8462ab2ff565577282b49d8665e4dc
MINIK HEILMANN <minikheilmann1908@gmail.com>
2023-11-29 15:15:19 +0100

    kinda works

--- Groenlaenderens_Kode/RockDetection/TestingRocks.py
+++ Groenlaenderens_Kode/RockDetection/TestingRocks.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import skimage.exposure as exposure
 
 # Load the image
 image = cv.imread("C:/Users/minik/Desktop/VSCode/GIt/P3-Gruppe-7/Groenlaenderens_Kode/RockDetection/Billeder/Image3.jpg")
@@ -10,33 +11,6 @@ cv.imshow("Image", image)
 
 # Convert to grayscale
 gray = cv.cvtColor(image2, cv.COLOR_BGR2GRAY)
-
-sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0, ksize=5)
-sobely = cv.Sobel(gray, cv.CV_64F, 0, 1, ksize=5)
-
-normSobelx = cv.normalize(sobelx, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-normSobely = cv.normalize(sobely, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-
-SquaredSobelx = cv.multiply(sobelx, sobelx)
-SquaredSobely = cv.multiply(sobely, sobely)
-
-SqrtSobel = cv.sqrt(SquaredSobelx + SquaredSobely + SquaredSobelx + SquaredSobely)
-Sobel = cv.add(normSobelx, normSobely)
-NormSqrtSob = cv.normalize(SqrtSobel, None, 0, 170, cv.NORM_MINMAX, cv.CV_8U)
-
-cv.imshow("SqrtSobelx", SqrtSobel)
-cv.imshow("Sobel", Sobel)
-cv.imshow("NormSqrtSob", NormSqrtSob)
-
-cv.imshow("SquaredSobelx", SquaredSobelx)
-cv.imshow("SquaredSobely", SquaredSobely)
-
-cv.imshow("Sobelx2", normSobelx)
-cv.imshow("Sobely2", normSobely)
-
-cv.imshow("Sobelx", sobelx)
-cv.imshow("Sobely", sobely)
-cv.waitKey(0)
 
 HSVImage = cv.cvtColor(image2, cv.COLOR_BGR2HSV)
 H, S, V = cv.split(HSVImage)
@@ -62,23 +36,29 @@ contours2 = cv.findContours(dilated2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[
 ellipse_img = np.zeros_like(image)
 roi = []
 ellipsis = []
-
+i = 1
 for cnt in contours2:
-    contour_area = cv.contourArea(cnt)
     ellipse = cv.fitEllipse(cnt)
+    
+    # Convert ellipse coordinates to integers
+    x_coord = int(ellipse[0][0])
+    y_coord = int(ellipse[0][1])
+
+    cv.putText(image2, str(i), (x_coord, y_coord), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     ellipsis.append(ellipse)
     cv.ellipse(image2, ellipse, (0, 255, 0), 2)
+    i += 1
 
     x, y, w, h = cv.boundingRect(cnt)
-    Box = cv.boxPoints(ellipse)
-    print(Box)
-    cv.rectangle(image2, (x-10, y-10), (x+w+10, y+h+10), (0, 0, 255), 2)
+    cv.rectangle(image2, (x, y), (x+w, y+h), (255, 0, 0), 2)
     roi.append(image[y-10:y+h+10, x-10:x+w+10])
 
 for img in roi:
     cv.destroyAllWindows()
     cv.imshow("ROI", img)
     cv.waitKey(0)
+
+print("Number of rocks: ", len(roi))
 
 cv.imshow("Image", image)
 cv.imshow("Image2", image2)
