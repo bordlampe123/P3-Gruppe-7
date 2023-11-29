5fffbd3cc321a46c8b6dd40e6e319a44a11b0876
MINIK HEILMANN <minikheilmann1908@gmail.com>
2023-11-29 13:51:12 +0100

    hmm

--- Grønlænderens_Kode/RockDetection/TestingRocks.py
+++ Grønlænderens_Kode/RockDetection/TestingRocks.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 # Load the image
-image = cv.imread("Grønlænderens_Kode/RockDetection/Billeder/Image3.jpg")
+image = cv.imread("C:/Users/minik/Desktop/VSCode/GIt/P3-Gruppe-7/Grønlænderens_Kode/RockDetection/Billeder/Image3.jpg")
 cv.imshow("Image", image)
 
 # Convert to grayscale
