621cdaa531545f23fdece0590767192ea3941c13
MINIK HEILMANN <minikheilmann1908@gmail.com>
2023-12-06 15:16:08 +0100

    Rock Detection basically done now

--- Groenlaenderens_Kode/RockDetection/RockDetection2.py
+++ Groenlaenderens_Kode/RockDetection/RockDetection2.py
@@ -3,160 +3,181 @@ import numpy as np
 import skimage.exposure as exposure
 
 #Load image and get image dimensions
-image = cv.imread("C:/Users/minik/Desktop/VSCode/GIt/P3-Gruppe-7/Groenlaenderens_Kode/RockDetection/Billeder/Image11.jpg")
+image = cv.imread("C:/Users/minik/Desktop/VSCode/GIt/P3-Gruppe-7/Groenlaenderens_Kode/RockDetection/Billeder/Image4.jpg")
 image2 = image.copy()
-img3 = image.copy()
+image3 = image.copy()
 img_h, img_w = image.shape[:2]
 
-# Convert to HSV and split into channels
-HSVImage = cv.cvtColor(image2, cv.COLOR_BGR2HSV)
-H, S, V = cv.split(HSVImage)
-cv.imshow("S", S)   
+def Preproccesing(image):
+    # Convert to HSV and split into channels
+    HSVImage = cv.cvtColor(image2, cv.COLOR_BGR2HSV)
+    H, S, V = cv.split(HSVImage) 
 
-# Thresholding the saturation channel
-thresholded = cv.threshold(S, 31.9, 255, cv.THRESH_BINARY)[1]
-#cv.imshow("Thresholded", thresholded)
+    # Thresholding the saturation channel,
+    thresholded = cv.threshold(S, 31.9, 255, cv.THRESH_BINARY)[1]
+    #cv.imshow("Thresholded", thresholded)
 
-# Dilate thresholded image and find contours
-dilated = cv.dilate(thresholded, (3, 3), iterations=3)
-cv.imshow("Dilated", dilated)
-contours = cv.findContours(thresholded, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+    # Dilate thresholded image and find contours
+    dilated = cv.dilate(thresholded, (3, 3), iterations=3)
+    #cv.imshow("Dilated", dilated)
+    contours = cv.findContours(thresholded, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
 
-#Finding small contours and drawing them on a mask
-small_contours = [cnt for cnt in contours if cv.contourArea(cnt) < 1000]
-mask = np.zeros_like(thresholded)
-cv.drawContours(mask, small_contours, -1, (255, 255, 255), -1)
-#cv.imshow("Mask", mask)
-#cv.waitKey(0)
+    #Finding small contours and drawing them on a mask
+    small_contours = [cnt for cnt in contours if cv.contourArea(cnt) < 1000]
+    mask = np.zeros_like(thresholded)
+    cv.drawContours(mask, small_contours, -1, (255, 255, 255), -1)
+    #cv.imshow("Mask", mask)
 
-#Subtracting the small contours from the thresholded image
-subtractedSmall = cv.subtract(thresholded, mask)
-dialated2 = cv.dilate(subtractedSmall, (3, 3), iterations=2)
-contours2 = cv.findContours(dialated2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
-contour_img = np.zeros_like(image)
+    #Subtracting the small contours from the thresholded image
+    subtractedSmall = cv.subtract(thresholded, mask)
+    dialated2 = cv.dilate(subtractedSmall, (3, 3), iterations=2)
+    contours2 = cv.findContours(dialated2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+    contour_img = np.zeros_like(image)
 
-#Drawing filled contours on the contour_img
-drawCont = cv.drawContours(contour_img, contours2, -1, (255, 255, 255), -1)
-cv.imshow("Contours", contour_img)
+    #Drawing filled contours on the contour_img
+    drawCont = cv.drawContours(contour_img, contours2, -1, (255, 255, 255), -1)
+    #cv.imshow("Contours", contour_img)
+    return contours2, drawCont, contour_img
 
-#Lists for sorting contours and ROI
+
+def watershed(image):
+    #checking if image contains anything
+    if np.sum(image) == 0:
+        return []
+    else:
+        #Morphological opening
+        kernel = np.ones((3, 3), np.uint8)
+        opening = cv.morphologyEx(image, cv.MORPH_OPEN, kernel, iterations=10)
+        opening = np.uint8(opening)
+        opening_gray = cv.cvtColor(opening, cv.COLOR_BGR2GRAY)
+
+        #Finding sure background and foreground through dilation and distance transform thresholding
+        sure_bg = cv.dilate(opening, kernel, iterations=2)
+        distance_transform = cv.distanceTransform(opening_gray, cv.DIST_L2, 5)
+        normalized_distance = exposure.rescale_intensity(distance_transform, out_range=(0, 255))
+        normalized_distance = normalized_distance.astype(np.uint8)
+        _, distThres = cv.threshold(distance_transform, 17, 255, cv.THRESH_BINARY)
+        sure_bg = cv.cvtColor(sure_bg, cv.COLOR_BGR2GRAY)
+        distThres = np.uint8(distThres)
+
+        #cv.imshow("Normalized Distance", normalized_distance)
+        #print("Sure_bg", sure_bg.shape)
+        #print("distThres", distThres.shape)
+        #cv.waitKey(0)
+        #Finding unknown region
+        unknown = cv.subtract(sure_bg, distThres)
+        #cv.imshow("sure_bg", sure_bg)
+        #cv.imshow("distThres", distThres)
+        #cv.imshow("Unknown", unknown)
+        #cv.waitKey(0)
+
+        #Labeling the markers, 
+        labels = cv.connectedComponents(distThres, connectivity=8, ltype=cv.CV_32S)[1]
+        labels = labels + 1
+
+        #Marking the unknown region with zero
+        labels[unknown == 255] = 0
+
+        mask = np.zeros_like(image)
+        mask2 = np.zeros_like(image)
+        #Watershed
+        labels = cv.watershed(contour_img, labels)
+        mask[labels == -1] = [255, 0, 0]
+        uniqueLabels = np.unique(labels)
+        print(uniqueLabels)
+        LabelContours = []
+        for label in uniqueLabels:
+            if label == -1 or label == 1:
+                continue
+            labelMask = np.zeros_like(image)
+            labelMask[labels == label] = 255
+            labelMask = cv.cvtColor(labelMask, cv.COLOR_BGR2GRAY)
+            contours = cv.findContours(labelMask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+            LabelContours.append(contours)
+    
+        return LabelContours
+        
+
+    
+contours2, drawCont, contour_img = Preproccesing(image)
+
+#Creating lists for sorting contours, and establishing thresholds
 InBoundRock = []
-InBoundCont = []
 OutBoundRock = []
 SortHull = []
+SingleRock = []
+MultipleRocks = []
 SolidityThres = 0.88
 EllipseThres = 0.80
-MultipleRocks = []
+LowerSolidityThres = 0.50
+LowerEllipseThres = 0.50
 
-
+#Sorting contours based on convex and ellipse solidity, and based on image boundaries
 for cnt in contours2:
-    Area = cv.contourArea(cnt)
     SortingConvexHull = cv.convexHull(cnt)
     SortingEllipse = cv.fitEllipse(SortingConvexHull)
+
+    Area = cv.contourArea(cnt)
     ConvexHullArea = cv.contourArea(SortingConvexHull)
-    SortingEllipseArea = (SortingEllipse[1][0]/2)*(SortingEllipse[1][1]/2)*np.pi
+    EllipseArea = (SortingEllipse[1][0]/2)*(SortingEllipse[1][1]/2)*np.pi
     Solidity = float(Area)/ConvexHullArea
-    SolidityEllipse = float(Area)/SortingEllipseArea
-    print("Solidity" + " " + str(Solidity))
-    print("Ellipse" + " " + str(SolidityEllipse))
+    SolidityEllipse = float(Area)/EllipseArea
+
     cv.ellipse(image2, SortingEllipse, (0, 255, 0), 2)
-    cv.imshow("Image2", image2)
-    cv.waitKey(0)
     if 15 < SortingEllipse[0][0] < img_w-15 and 15 < SortingEllipse[0][1] < img_h-15:
         InBoundRock.append(SortingEllipse)
-        InBoundCont.append(cnt)
         SortHull.append(SortingConvexHull)
         if Solidity < SolidityThres and SolidityEllipse < EllipseThres:
-            MultipleRocks.append(SortingEllipse)
+            MultipleRocks.append(cnt)
+        else:
+            SingleRock.append(cnt)
     else:
         OutBoundRock.append(SortingEllipse)
 
 
+MultipleRockImg = np.zeros_like(image)
+SingleRockImg = np.zeros_like(image)
 
-cv.rectangle(image2, (15, 15), (img_w-15, img_h-15), (0, 0, 255), 2)
+cv.drawContours(MultipleRockImg, MultipleRocks, -1, (255, 255, 255), -1)
+cv.drawContours(SingleRockImg, SingleRock, -1, (255, 255, 255), -1)
 
-#print("Inbound Rock Count:", len(InBoundRock))
-#print("Outbound Rock Count:", len(OutBoundRock))
-cv.waitKey(0)
-
-cv.imshow("drawCont", drawCont) 
-cv.waitKey(0)
-#Morphological opening
-kernel = np.ones((3, 3), np.uint8)
-opening = cv.morphologyEx(contour_img, cv.MORPH_OPEN, kernel, iterations=10)
-opneing = np.uint8(opening)
-cv.imshow("Opening", opening)
-opening_gray = cv.cvtColor(opening, cv.COLOR_BGR2GRAY)
-
-#Finding sure background and foreground through dilation and distance transform thresholding
-sure_bg = cv.dilate(opening, kernel, iterations=2)
-cv.imshow("Sure_bg", sure_bg)
-
-#Distance transform, distance from each pixel to the nearest zero pixel, normalized to fit in 0-255
-distance_transform = cv.distanceTransform(opening_gray, cv.DIST_L2, 5)
-normalized_distance = exposure.rescale_intensity(distance_transform, out_range=(0, 255))
-normalized_distance = normalized_distance.astype(np.uint8)
-cv.imshow("Normalized Distance", normalized_distance)
-#cv.imshow("Distance Transform", distance_transform)
-#cv.waitKey(0)
-
-_, distThres = cv.threshold(distance_transform, 17, 255, cv.THRESH_BINARY)
-cv.imshow("Distance Threshold", distThres)
+cv.imshow("MultipleRockImg", MultipleRockImg)
+cv.imshow("SingleRockImg", SingleRockImg)
 
 
-sure_bg = cv.dilate(opening, kernel, iterations=2)
-cv.imshow("Sure_bg", sure_bg)
-sure_bGray = cv.cvtColor(sure_bg, cv.COLOR_BGR2GRAY)
+LabelContours = watershed(MultipleRockImg)
+AllEllipse = []
+AllContours = []
+FinalImg = np.zeros_like(image)
+FinalImg[SingleRockImg == 255] = 255
+AllContours.extend(SingleRock)
 
-sure_bGray = np.uint8(sure_bGray)
-distThres = np.uint8(distThres)
+for contours in LabelContours:
+    AllContours.extend(contours)
+    cv.drawContours(FinalImg, contours, -1, (255, 255, 255), -1)
+    
+for cnt in AllContours:
+    ellipse = cv.fitEllipse(cnt)
+    print(ellipse)
+    AllEllipse.append(ellipse)
+    cv.ellipse(image3, ellipse, (0, 255, 0), 2)
 
+#Sorting ellipses based on longest axis
+for ellipse in AllEllipse:
+    SortedEllipse = sorted(AllEllipse, key=lambda x: max(x[1]), reverse=True)
 
-unknown = cv.subtract(sure_bGray, distThres)
-cv.imshow("Unknown", unknown)
-
-labels = cv.connectedComponents(distThres, connectivity=8, ltype=cv.CV_32S)[1]
-labelVis = cv.applyColorMap(np.uint8(labels), cv.COLORMAP_JET)
-labels = labels + 1
-labels[unknown == 255] = 0
-
-labels = cv.watershed(image2, labels)
-cv.imshow("Image2", np.uint8(labels))
-image2[labels == -1] = [255, 0, 0]
-
-UnLabels = np.unique(labels)
-
-AllCont = []
-
-for i in range(len(UnLabels)):
-    if i == 0 or i == -1 or i == 1:
-        continue
-    else:
-        mask = np.zeros_like(image2)
-        mask[labels == i] = 255
-        maskThresh = cv.threshold(mask, 1, 255, cv.THRESH_BINARY)[1]
-        maskThresh = np.uint8(maskThresh)
-        maskThresh = cv.cvtColor(maskThresh, cv.COLOR_BGR2GRAY)
-        contours, _ = cv.findContours(maskThresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        AllCont.extend(contours)
-        drawCont = cv.drawContours(image2, contours, -1, (0, 255, 0), 2)
-        continue
-
-AllEllips = []
-for cont in AllCont:
-    hull = cv.convexHull(cont)
-    ellipsis = cv.fitEllipse(hull)
-    cv.ellipse(img3, ellipsis, (0, 255, 0), 2)
-    AllEllips.append(ellipsis)
+def GetList(x,y):
+    if x == 0 and y == 0:
+        return SortedEllipse
+    if x == 1 and y == 0:
+        return SortedEllipse[0]
+    if y == 1:
+        return SortedEllipse[x]
 
 
-print("Rock Count:", len(AllEllips))
-
-
-cv.imshow("labels2", np.uint8(labels))
 
 cv.imshow("Image2", image2)
-cv.imshow("Image3", img3)
-cv.imshow("Labels", labelVis)
+cv.imshow("Image3", image3)
+cv.imshow("FinalImg", FinalImg)
 cv.waitKey(0)
 cv.destroyAllWindows()
